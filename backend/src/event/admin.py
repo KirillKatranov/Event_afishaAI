3090820d57979daea0ee3dfce72477adb00fd023
Artem Bogatov <artembbogatov@yandex.ru>
2025-06-14 17:56:57 +0300

    feat: fix

--- backend/src/event/admin.py
+++ backend/src/event/admin.py
@@ -1,38 +1,47 @@
 from django.contrib import admin
-from event.models import (
+from frameworks_and_drivers.django.parsing.data_manager.models import (
     Content,
     Tags,
     Like,
     User,
     MacroCategory,
-    UserCategoryPreference,
-    Feedback,
 )
 
 
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
     list_display = (
-        "id",
         "name",
-        "description",
-        "image",
-        "contact",
+        "city",
         "date_start",
         "date_end",
-        "time",
-        "location",
-        "cost",
-        "city",
-        "unique_id",
+        "event_type",
+        "publisher_type",
         "get_tags",
         "get_macro",
+        "created",
     )
+    list_filter = (
+        "city",
+        "event_type",
+        "publisher_type",
+        "date_start",
+        "created",
+    )
+    search_fields = (
+        "name",
+        "description",
+        "location",
+        "unique_id",
+    )
+    readonly_fields = ("unique_id", "created", "updated")
+    date_hierarchy = "date_start"
+    list_per_page = 25
 
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description")
+    list_display = ("name", "description")
 
 
 @admin.register(Like)
@@ -40,27 +49,11 @@ class LikeAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = (
-        "user",
-        "message",
-    )
-
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "username",
-        "city",
-    )
+    list_display = ("username",)
 
 
 @admin.register(MacroCategory)
 class MacroCategoryAdmin(admin.ModelAdmin):
     list_display = ("name",)
-
-
-@admin.register(UserCategoryPreference)
-class UserCategoryPreferenceAdmin(admin.ModelAdmin):
-    list_display = ("user", "tag")
