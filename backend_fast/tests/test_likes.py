0b596e8797a8a72599e4da16969eac164406caab
Кирилл Катранов <katranovrpk@gmail.com>
2025-07-08 13:59:33 +0300

    Add some tests in likes/dislike

--- backend_fast/tests/test_likes.py
+++ backend_fast/tests/test_likes.py
@@ -1,8 +1,12 @@
 
  
 
+from models import Like
+from tests.conftest import TestingSessionLocal
+
+
 class TestLikes:
-    def test_set_like(self, client, create_test_user, create_test_content):
+    def test_set_like(self, client, create_test_user1, create_test_content1):
         response = client.post(
             "/api/v1/like", json={"username": "TestUser", "content_id": 1}
         )
@@ -10,9 +14,12 @@ class TestLikes:
 
         assert response.status_code == 200
         assert data == {"user": "TestUser", "content": 1, "value": True}
+        with TestingSessionLocal() as db:
+            like = db.query(Like).filter_by(user_id=1).one()
+            assert like.value == True
 
 
-    def test_set_dislike(self, client, create_test_user, create_test_content):
+    def test_set_dislike(self, client, create_test_user1, create_test_content1):
         response = client.post(
             "/api/v1/dislike", json={"username": "TestUser", "content_id": 1}
         )
@@ -20,9 +27,12 @@ class TestLikes:
 
         assert response.status_code == 200
         assert data == {"user": "TestUser", "content": 1, "value": False}
+        with TestingSessionLocal() as db:
+            like = db.query(Like).filter_by(user_id=1).one()
+            assert like.value == False
 
 
-    def test_set_like_without_content(self, client, create_test_user, create_test_content):
+    def test_set_like_without_content(self, client, create_test_user1, create_test_content1):
         response = client.post(
             "/api/v1/like", json={"username": "TestUser", "content_id": 2}
         )
@@ -31,7 +41,7 @@ class TestLikes:
         assert response.status_code == 404
         assert data["detail"] == "Content not found"
 
-    def test_set_dislike_without_content(self, client, create_test_user, create_test_content):
+    def test_set_dislike_without_content(self, client, create_test_user1, create_test_content1):
         response = client.post(
             "/api/v1/like", json={"username": "TestUser", "content_id": 2}
         )
